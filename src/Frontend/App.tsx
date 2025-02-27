ce1bd6bdbf13ae6dd0a5159da8c3f8ece2dc5fbc
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-27 13:48:02 -0300

    librería Swiper instalada para manejar el carrusel de la sección Testimonials

--- src/Frontend/App.tsx
+++ src/Frontend/App.tsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import PricingPlans from "./components/PricingPlans/PricingPlans";
+import Testimonials from "./components/Testimonials/Testimonials";
 import WhatWeDo from "./components/WhatWeDo/WhatWeDo";
 import WhoWeAre from "./components/WhoWeAre/WhoWeAre";
 
@@ -16,6 +17,7 @@ function App() {
       <WhoWeAre />
       <WhatWeDo />
       <PricingPlans />
+      <Testimonials />
       <Footer />
     </>
   );
