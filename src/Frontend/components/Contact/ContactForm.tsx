cabd202b1572550c9d76a804f2c45029df18637f
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-02-28 16:22:58 -0300

    useFormInput separado de ContactForm para mejorar legibilidad en componentes

--- src/Frontend/components/Contact/ContactForm.tsx
+++ src/Frontend/components/Contact/ContactForm.tsx
@@ -1,34 +1,8 @@
-import { useState } from "react";
+import { useFormInput } from "../../hooks/useFormInput";
 import "./ContactForm.css";
 
 export function ContactForm() {
-  const [errors, setErrors] = useState<{ [key: string]: string }>({});
-
-  const handleBlur = (
-    e: React.FocusEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-    >
-  ) => {
-    const { name, value } = e.target;
-    if (!value) {
-      setErrors((prevErrors) => ({
-        ...prevErrors,
-        [name]: `El ${name} es obligatorio.`,
-      }));
-    }
-  };
-
-  const handleFocus = (
-    e: React.FocusEvent<
-      HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
-    >
-  ) => {
-    const { name } = e.target;
-    setErrors((prevErrors) => ({
-      ...prevErrors,
-      [name]: "",
-    }));
-  };
+  const { errors, handleBlur, handleFocus } = useFormInput();
 
   return (
     <form
