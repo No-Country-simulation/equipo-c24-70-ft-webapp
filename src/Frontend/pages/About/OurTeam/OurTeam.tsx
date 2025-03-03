09d8463c22dfa0fd51d5a4836b52308209c6b365
Emmanuel Cruz <emmanuel-cruz-dev@users.noreply.github.com>
2025-03-03 17:10:33 -0300

    agregando imagenes, nombres y rol a OutTeam

--- src/Frontend/pages/About/OurTeam/OurTeam.tsx
+++ src/Frontend/pages/About/OurTeam/OurTeam.tsx
@@ -1,20 +1,56 @@
+import { FC } from "react";
 import TeamImg1 from "../../../../assets/teams-img1.png";
-import TeamImg2 from "../../../../assets/teams-img2.png";
-import TeamImg3 from "../../../../assets/teams-img3.png";
-import TeamImg4 from "../../../../assets/teams-img4.png";
+import TeamImg2 from "../../../../assets/alexander-castillo.jpg";
+import TeamImg3 from "../../../../assets/emmanuel-cruz.jpg";
+import TeamImg4 from "../../../../assets/antonella-heimen.jpg";
 import SocialIcons from "../../../components/SocialIcons/SocialIcons";
 
-const TeamCard = () => {
+interface TeamCardProps {
+  img: string;
+  name: string;
+  occupation: string;
+}
+
+const teamMembers = [
+  {
+    id: 1,
+    img: TeamImg1,
+    name: "Nataly Castañeda",
+    occupation: "Fullstack Developer",
+  },
+  {
+    id: 2,
+    img: TeamImg2,
+    name: "Alexander Castillo",
+    occupation: "Fullstack Developer",
+  },
+  {
+    id: 3,
+    img: TeamImg3,
+    name: "Emmanuel Cruz",
+    occupation: "Frontend Developer",
+  },
+  {
+    id: 4,
+    img: TeamImg4,
+    name: "Antonella Heimen",
+    occupation: "QA Tester",
+  },
+];
+
+const TeamCard: FC<TeamCardProps> = ({ img, name, occupation }) => {
   return (
     <article className="text-center">
       <header className="relative mb-8">
-        <img className="" src={TeamImg1} alt="" />
+        <figure className="rounded-lg overflow-hidden">
+          <img className="w-full object-cover" src={img} alt="" />
+        </figure>
         <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 translate-y-1/2">
           <SocialIcons borderRadius={true} />
         </div>
       </header>
-      <h2>Morgan Cooper</h2>
-      <p className="uppercase">CEO & Founder</p>
+      <h2 className="text-xl font-bold">{name}</h2>
+      <p className="uppercase text-neutral-600">{occupation}</p>
     </article>
   );
 };
@@ -24,7 +60,7 @@ function OurTeam() {
     <section className="container">
       <article className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center py-8">
         <article>
-          <h1 className="text-lg uppercase mb-2">Out Team</h1>
+          <h1 className="text-lg uppercase mb-2">Our Team</h1>
           <h2 className="text-3xl lg:text-4xl xl:text-5xl font-bold mb-5">
             Expert Team Who are Behind the Security
           </h2>
@@ -43,11 +79,10 @@ function OurTeam() {
             </p>
           </div>
         </article>
-        <aside className="grid grid-cols-2 gap-8 mx-auto">
-          <TeamCard />
-          <img className="" src={TeamImg2} alt="" />
-          <img className="" src={TeamImg3} alt="" />
-          <img className="" src={TeamImg4} alt="" />
+        <aside className="grid grid-cols-2 gap-x-8 gap-y-4 mx-auto">
+          {teamMembers.map((member) => {
+            return <TeamCard key={member.id} {...member} />;
+          })}
         </aside>
       </article>
     </section>
